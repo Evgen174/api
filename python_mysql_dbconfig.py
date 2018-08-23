cf8d5cf03bbcfb388db01555a1880dd72a7df1ff
Evgen174 <40502881+Evgen174@users.noreply.github.com>
2018-08-24 01:11:02 +0300

    Update python_mysql_dbconfig.py

--- python_mysql_dbconfig.py
+++ python_mysql_dbconfig.py
@@ -1,5 +1,5 @@
 import pymysql
-
+#from configparser import ConfigParser
 
 
 def getconnection():
@@ -9,3 +9,21 @@ def getconnection():
                                  passwd='1qaz@WSX',
                                  db='link_short')
     return connection
+
+
+ 
+ 
+#def read_db_config(filename='config.ini', section='mysql'):
+
+    #parser = ConfigParser()
+    #parser.read(filename)
+ 
+    #db = {}
+    #if parser.has_section(section):
+        #items = parser.items(section)
+        #for item in items:
+            #db[item[0]] = item[1]
+    #else:
+        #raise Exception('{0} not found in the {1} file'.format(section, filename))
+ 
+    #return db
